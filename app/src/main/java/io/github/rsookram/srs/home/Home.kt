62db9cd01e665e88fe957250480a6e36ead84d55
rsookram <rashadsookram@gmail.com>
2021-06-19 17:38:39 -0400

    Add dialog for creating a deck

--- app/src/main/java/io/github/rsookram/srs/home/Home.kt
+++ app/src/main/java/io/github/rsookram/srs/home/Home.kt
@@ -1,31 +1,50 @@
 package io.github.rsookram.srs.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import io.github.rsookram.srs.DeckWithCount
 import io.github.rsookram.srs.R
 import io.github.rsookram.srs.ui.BottomBar
 import io.github.rsookram.srs.ui.TopLevelScreen
 import io.github.rsookram.srs.ui.theme.SrsTheme
+import kotlinx.coroutines.delay
+
+typealias DeckName = String
 
 @Composable
-fun Home(decks: List<DeckWithCount>) {
+fun Home(decks: List<DeckWithCount>, onCreateDeckClick: (DeckName) -> Unit) {
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(stringResource(R.string.app_name)) })
@@ -39,27 +58,37 @@ fun Home(decks: List<DeckWithCount>) {
             }
         }
     ) {
+        var showCreateDeckDialog by rememberSaveable { mutableStateOf(false) }
+
         LazyColumn {
             items(decks) { deck ->
                 DeckItem(deck = deck)
             }
 
-            item { AddDeckItem() }
+            item {
+                CreateDeckItem(Modifier.clickable { showCreateDeckDialog = true })
+            }
+        }
+
+        if (showCreateDeckDialog) {
+            CreateDeckDialog(
+                onCreateDeckClick,
+                onDismiss = { showCreateDeckDialog = false },
+            )
         }
     }
 }
 
 @Preview
 @Composable
-private fun HomePreview() {
-    SrsTheme {
-        Home(
-            decks = listOf(
-                DeckWithCount(id = 1, name = "中文", scheduledCardCount = 0),
-                DeckWithCount(id = 2, name = "日本語", scheduledCardCount = 12),
-            )
-        )
-    }
+private fun HomePreview() = SrsTheme {
+    Home(
+        decks = listOf(
+            DeckWithCount(id = 1, name = "中文", scheduledCardCount = 0),
+            DeckWithCount(id = 2, name = "日本語", scheduledCardCount = 12),
+        ),
+        onCreateDeckClick = {},
+    )
 }
 
 @Composable
@@ -83,7 +112,7 @@ private fun DeckItemPreview() = SrsTheme {
 }
 
 @Composable
-fun AddDeckItem(modifier: Modifier = Modifier) {
+fun CreateDeckItem(modifier: Modifier = Modifier) {
     Row(modifier.heightIn(min = 56.dp), verticalAlignment = Alignment.CenterVertically) {
         Icon(
             Icons.Default.Add,
@@ -92,7 +121,7 @@ fun AddDeckItem(modifier: Modifier = Modifier) {
         )
 
         Text(
-            "Add deck",
+            "Create deck",
             Modifier
                 .weight(1f)
                 .padding(horizontal = 16.dp),
@@ -102,6 +131,57 @@ fun AddDeckItem(modifier: Modifier = Modifier) {
 
 @Preview
 @Composable
-private fun AddDeckItemPreview() = SrsTheme {
-    AddDeckItem()
+private fun CreateDeckItemPreview() = SrsTheme {
+    CreateDeckItem()
+}
+
+@Composable
+private fun CreateDeckDialog(
+    onCreateClick: (DeckName) -> Unit,
+    onDismiss: () -> Unit,
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Card {
+            var deckName by rememberSaveable { mutableStateOf("") }
+
+            Column(Modifier.padding(16.dp)) {
+                Text(text = "Create Deck", style = MaterialTheme.typography.h6)
+
+                val focusRequester = FocusRequester()
+
+                LaunchedEffect(Unit) {
+                    delay(16) // Workaround to make keyboard show
+                    focusRequester.requestFocus()
+                }
+
+                OutlinedTextField(
+                    value = deckName,
+                    onValueChange = { deckName = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                        .focusRequester(focusRequester),
+                    label = { Text("Name") },
+                )
+
+                Row(
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    TextButton(onClick = onDismiss) {
+                        Text(text = "CANCEL")
+                    }
+
+                    TextButton(
+                        onClick = {
+                            onCreateClick(deckName)
+                            onDismiss()
+                        }
+                    ) {
+                        Text(text = "CREATE")
+                    }
+                }
+            }
+        }
+    }
 }
