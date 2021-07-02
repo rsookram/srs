5d939efb8251faaa77a3def207b7c8ec9dd4e716
rsookram <rashadsookram@gmail.com>
2021-07-02 19:16:59 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
 
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
@@ -46,5 +46,4 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${rootProject.extra["coroutines_version"]}")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${rootProject.extra["coroutines_version"]}")
-
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
 
-    val compose_version by extra("1.0.0-beta09")
+    val compose_version by extra("1.0.0-rc01")
     val coroutines_version by extra("1.5.0")
     val sqldelight_version by extra("1.5.0")
 
@@ -10,7 +10,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta04")
+        classpath("com.android.tools.build:gradle:7.0.0-beta05")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
 
         classpath("com.squareup.sqldelight:gradle-plugin:$sqldelight_version")
