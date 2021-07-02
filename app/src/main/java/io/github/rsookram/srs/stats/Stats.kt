6ed789e0a4222177a89d61d0a55719248b992e25
rsookram <rashadsookram@gmail.com>
2021-07-01 22:17:11 -0400

    Add top-level composable for stats screen

---
+++ app/src/main/java/io/github/rsookram/srs/stats/Stats.kt
@@ -0,0 +1,142 @@
+package io.github.rsookram.srs.stats
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.rsookram.srs.DeckStats
+import io.github.rsookram.srs.GlobalStats
+import io.github.rsookram.srs.ui.BottomBar
+import io.github.rsookram.srs.ui.TopLevelScreen
+import io.github.rsookram.srs.ui.theme.SrsTheme
+
+@Composable
+fun Stats(global: GlobalStats, decks: List<DeckStats>) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Stats") })
+        },
+        bottomBar = {
+            BottomBar(selected = TopLevelScreen.STATS, onItemClick = { /*TODO*/ })
+        },
+    ) {
+        LazyColumn {
+            item { GlobalCard(global) }
+
+            items(decks) { deck -> DeckCard(deck) }
+        }
+    }
+}
+
+@Composable
+private fun GlobalCard(global: GlobalStats) {
+    Card(Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)) {
+        Column(Modifier.padding(16.dp)) {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text("All", style = MaterialTheme.typography.h6)
+                Text(
+                    "Total ${global.activeCount + global.suspendedCount + global.leechCount}",
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+
+            Text(
+                "${global.activeCount} active, " +
+                    "${global.suspendedCount} suspended, " +
+                    "${global.leechCount} leeches",
+                Modifier.padding(top = 16.dp),
+            )
+
+            Text("Review tomorrow: ${global.forReviewCount}", Modifier.padding(top = 8.dp))
+        }
+    }
+}
+
+@Composable
+private fun DeckCard(deck: DeckStats) {
+    Card(Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+        Column(Modifier.padding(16.dp)) {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(deck.name, style = MaterialTheme.typography.h6)
+                Text(
+                    "Total ${deck.activeCount + deck.suspendedCount + deck.leechCount}",
+                    style = MaterialTheme.typography.body2,
+                )
+            }
+
+            Text(
+                "${deck.activeCount} active, " +
+                    "${deck.suspendedCount} suspended, " +
+                    "${deck.leechCount} leeches",
+                Modifier.padding(top = 16.dp),
+            )
+
+            val answerCount = deck.correctCount + deck.wrongCount
+            val percentCorrect = deck.correctCount * 100 / answerCount
+            Text(
+                "Past month accuracy: $percentCorrect% (${deck.correctCount} / $answerCount)",
+                Modifier.padding(top = 8.dp),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun StatsPreview() = SrsTheme {
+    Stats(
+        global = GlobalStats(
+            activeCount = 1375,
+            suspendedCount = 278,
+            leechCount = 0,
+            forReviewCount = 37,
+        ),
+        decks = listOf(
+            DeckStats(
+                name = "prog",
+                activeCount = 1177,
+                suspendedCount = 266,
+                leechCount = 0,
+                correctCount = 391,
+                wrongCount = 17,
+            ),
+            DeckStats(
+                name = "中文",
+                activeCount = 60,
+                suspendedCount = 7,
+                leechCount = 0,
+                correctCount = 40,
+                wrongCount = 1,
+            ),
+            DeckStats(
+                name = "日本語",
+                activeCount = 138,
+                suspendedCount = 5,
+                leechCount = 0,
+                correctCount = 52,
+                wrongCount = 1,
+            ),
+        ),
+    )
+}
