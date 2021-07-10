dbc315018fd89fc796c3ee6efbb5840d341734d0
rsookram <rashadsookram@gmail.com>
2021-07-10 15:52:42 -0400

    Update design of card in Browser

--- app/src/main/java/io/github/rsookram/srs/browser/Browser.kt
+++ app/src/main/java/io/github/rsookram/srs/browser/Browser.kt
@@ -1,14 +1,19 @@
 package io.github.rsookram.srs.browser
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
@@ -35,12 +40,34 @@ fun Browser(
             items(cardItems) { item ->
                 val modifier = Modifier.heightIn(min = 48.dp)
                 if (item != null) {
-                    // TODO: background colour for leech
-                    Text(item.front, modifier.clickable { onCardClick(item.id) })
+                    Card(modifier, item, onCardClick)
                 } else {
                     Spacer(modifier)
                 }
             }
         }
     }
+}
+
+@Composable
+private fun Card(
+    modifier: Modifier,
+    card: BrowserCard,
+    onClick: (cardId: Long) -> Unit
+) {
+    Text(
+        card.front,
+        Modifier
+            .clickable { onClick(card.id) }
+            .background(
+                if (card.isLeech) {
+                    MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+                } else {
+                    Color.Transparent
+                }
+            )
+            .fillMaxWidth()
+            .then(modifier)
+            .padding(16.dp)
+    )
 }
