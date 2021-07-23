00d61a9a636f7a59f018a421a55778c546001773
rsookram <rashadsookram@gmail.com>
2021-07-22 21:23:00 -0400

    Create string res for confirm card deletion

--- app/src/main/java/io/github/rsookram/srs/ui/ConfirmDeleteCardDialog.kt
+++ app/src/main/java/io/github/rsookram/srs/ui/ConfirmDeleteCardDialog.kt
@@ -4,21 +4,23 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import io.github.rsookram.srs.R
 
 @Composable
 fun ConfirmDeleteCardDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
     AlertDialog(
         onDismissRequest = onDismiss,
-        title = { Text("Delete Card") },
-        text = { Text("Are you sure you want to delete this card?") },
+        title = { Text(stringResource(R.string.delete_card_title)) },
+        text = { Text(stringResource(R.string.delete_card_confirmation)) },
         confirmButton = {
             TextButton(onClick = onConfirm) {
-                Text("Confirm")
+                Text(stringResource(R.string.confirm_changes_button))
             }
         },
         dismissButton = {
             TextButton(onClick = onDismiss) {
-                Text("Dismiss")
+                Text(stringResource(R.string.dismiss_dialog_button))
             }
         }
     )
