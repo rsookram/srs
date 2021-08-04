0dfb4764c336cc2ba2455d9025eebec8bf49568e
Rashad Sookram <rashadsookram@gmail.com>
2021-08-04 19:38:35 -0400

    Update dependencies
    Compose 1.0.1 supports Kotlin 1.5.21. The other libraries are being 
    updated for related reasons.
    
    (#8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,10 +84,10 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.activity:activity-compose:1.3.0")
+    implementation("androidx.activity:activity-compose:1.3.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha06")
     implementation("androidx.paging:paging-compose:1.0.0-alpha12")
 
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
 
-    val accompanist_version by extra("0.15.0")
-    val compose_version by extra("1.0.0")
-    val coroutines_version by extra("1.5.0")
+    val accompanist_version by extra("0.16.0")
+    val compose_version by extra("1.0.1")
+    val coroutines_version by extra("1.5.1")
     val hilt_version by extra("2.38.1")
     val sqldelight_version by extra("1.5.1")
 
@@ -15,7 +15,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
 
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqldelight_version")
