27f58c6e719dc300f4bd7bfe476807385a96b2da
Rashad Sookram <rashadsookram@gmail.com>
2021-08-06 17:50:00 -0400

    Fix existing lint warnings and run on CI
    * Configure Lint
    * Specify fullBackupContent to satisfy lint
    * Simplify resource qualifiers since minSdkVersion is 28
    * Disable ButtonCase lint check
    
    (#10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,18 @@ android {
     }
 
     lint {
-        // TODO: Run lint on CI
+        // Lint is run on CI
         isCheckReleaseBuilds = false
+
+        textReport = true
+
+        isWarningsAsErrors = true
+        isAbortOnError = true
+
+        // This check goes against the Material Design guidelines:
+        // https://material.io/components/dialogs#actions
+        // https://material.io/components/buttons#anatomy
+        disable("ButtonCase")
     }
 
     packagingOptions.resources {
