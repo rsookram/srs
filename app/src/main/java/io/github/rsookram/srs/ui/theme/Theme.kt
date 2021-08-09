18d4bbf45b3634071da14760445a5f72679dfae2
rsookram <rashadsookram@gmail.com>
2021-08-08 20:47:14 -0400

    Animate between light and dark mode

--- app/src/main/java/io/github/rsookram/srs/ui/theme/Theme.kt
+++ app/src/main/java/io/github/rsookram/srs/ui/theme/Theme.kt
@@ -1,31 +1,48 @@
 package io.github.rsookram.srs.ui.theme
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
-
-private val DarkColorPalette =
-    darkColors(
-        primary = Color(0xFFFFFFFF),
-        primaryVariant = Color(0xFFE0E0E0),
-        secondary = Color(0xFF757575)
-    )
-
-private val LightColorPalette =
-    lightColors(
-        primary = Color(0xFF121212),
-        primaryVariant = Color(0xFF000000),
-        secondary = Color(0xFFE0E0E0),
-        secondaryVariant = Color(0xFFE0E0E0)
-    )
+import androidx.compose.ui.graphics.luminance
 
 @Composable
-fun SrsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun SrsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val spec = tween<Color>(durationMillis = 600)
+
+    val primary by animateColorAsState(Color(if (darkTheme) 0xFFFFFFFF else 0xFF121212), spec)
+    val primaryVariant by animateColorAsState(
+        Color(if (darkTheme) 0xFFE0E0E0 else 0xFF000000),
+        spec,
+    )
+    val secondary by animateColorAsState(Color(if (darkTheme) 0xFF757575 else 0xFFE0E0E0), spec)
+    val background by animateColorAsState(Color(if (darkTheme) 0xFF121212 else 0xFFFFFFFF), spec)
+    val error by animateColorAsState(Color(if (darkTheme) 0xFFCF6679 else 0xFFB00020), spec)
+    val onPrimary by animateColorAsState(if (darkTheme) Color.Black else Color.White, spec)
+    val onSecondary by animateColorAsState(if (darkTheme) Color.Black else Color.Black, spec)
+    val onBackground by animateColorAsState(if (darkTheme) Color.White else Color.Black, spec)
+    val onError by animateColorAsState(if (darkTheme) Color.Black else Color.White, spec)
+
     MaterialTheme(
-        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+        Colors(
+            primary,
+            primaryVariant,
+            secondary,
+            secondaryVariant = secondary,
+            background,
+            surface = background,
+            error,
+            onPrimary,
+            onSecondary,
+            onBackground,
+            onSurface = onBackground,
+            onError,
+            isLight = onPrimary.luminance() > 0.5f,
+        ),
         content = content,
     )
 }
