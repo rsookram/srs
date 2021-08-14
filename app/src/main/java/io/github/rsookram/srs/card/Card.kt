c3cdce9009045dd00c414af9587e81184771a782
rsookram <rashadsookram@gmail.com>
2021-08-14 18:46:33 -0400

    Replace Row with Box
    since it only had one child composable

--- app/src/main/java/io/github/rsookram/srs/card/Card.kt
+++ app/src/main/java/io/github/rsookram/srs/card/Card.kt
@@ -3,7 +3,6 @@ package io.github.rsookram.srs.card
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -80,7 +79,7 @@ fun Card(
                         )
                     ),
             ) {
-                Row(Modifier.width(68.dp), verticalAlignment = Alignment.CenterVertically) {
+                Box(Modifier.width(68.dp)) {
                     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
                         IconButton(onClick = onUpClick) {
                             Icon(
